315bfa59243b0405f2def638f02a9b55f0a00a59
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-28 23:16:46 -0700

    added feature to know if current user is online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  after_action :user_activity
+
   include Pundit
   protect_from_forgery
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -17,5 +19,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized #pundit
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def user_activity
+    current_user.try :touch
   end
 end
