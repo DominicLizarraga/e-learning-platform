857782678938633ec80c3a946d0cfe6381f44cf3
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-11-14 18:10:22 -0800

    small changes to main page

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -20,6 +20,8 @@ class Enrollment < ApplicationRecord
   end
 
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
+  scope :reviewed, -> { where.not(review: [0, nil, ""]) }
+
 
   after_save do
     unless rating.nil? || rating.zero?
